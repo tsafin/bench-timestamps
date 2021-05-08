1f606fa764684cb131b5ccb694a1589209dcb146
Timur Safin <tsafin@tarantool.org>
2021-05-08 14:18:24 +0300

    c-dt bench fixed
    Fixed bench-cdt.cpp to correctly parse full date, time, timezone
    string, and not only date part (which provided it 3x speedup before)

--- benchs/bench-cdt.cpp
+++ benchs/bench-cdt.cpp
@@ -78,18 +78,73 @@ const char * tests[] = {
 
 #define DIM(a) (sizeof(a) / sizeof(a[0]))
 
+// p5-time/moment/src/moment_pare.c: parse_string_strict()
+static int
+parse_datetime_extended(const char *str, size_t len,
+			int64_t *sp, int64_t *np, int64_t *op)
+{
+	size_t n;
+	dt_t dt;
+	int sod, nanosecond, offset;
+	bool extended;
+
+	n = dt_parse_iso_date(str, len, &dt);
+	if (!n || n == len)
+		return 1;
+
+	/*
+	* 0123456789
+	* 2012-12-14
+	*/
+	extended = str[4] == '-';
+	if (str[n++] != 'T')
+		return 1;
+
+	str += n;
+	len -= n;
+
+	if (extended)
+		n = dt_parse_iso_time_extended(str, len, &sod, &nanosecond);
+	else
+		n = dt_parse_iso_time_basic(str, len, &sod, &nanosecond);
+
+	if (!n || n == len)
+		return 1;
+
+	str += n;
+	len -= n;
+
+	if (extended)
+		n = dt_parse_iso_zone_extended(str, len, &offset);
+	else
+		n = dt_parse_iso_zone_basic(str, len, &offset);
+
+	if (!n || n != len)
+		return 1;
+
+	*sp = ((int64_t)dt_rdn(dt) - 719163) * 86400 + sod - offset * 60;
+	*np = nanosecond;
+	*op = offset;
+	return 0;
+}
+
 static void ParseTimeStamps()
 {
 	size_t index;
-	dt_t dt_expected;
-	size_t rc = dt_parse_iso_date(sample, sizeof(sample) - 1, &dt_expected);
-	assert(rc > 0);
+	int64_t secs_expected;
+	int64_t nanosecs;
+	int64_t ofs;
+	int rc = parse_datetime_extended(sample, sizeof(sample) - 1,
+					 &secs_expected, &nanosecs, &ofs);
+	assert(rc == 0);
+	assert(nanosecs == 0);
 
 	for (index = 0; index < DIM(tests); index++) {
-		dt_t dt_actual;
-		rc = dt_parse_iso_date(tests[index], strlen(tests[index]), &dt_actual);
-		assert(rc > 0);
-		assert(dt_actual == dt_expected);
+		int64_t secs;
+		int rc = parse_datetime_extended(sample, sizeof(sample) - 1,
+						&secs, &nanosecs, &ofs);
+		assert(rc == 0);
+		assert(secs == secs_expected);
 	}
 }
 
@@ -102,11 +157,13 @@ BENCHMARK(CDT_Parse);
 static void Parse1()
 {
 	const char civil_string[] = "2015-02-18T10:50:31.521345123+10:00";
-	dt_t dt_expected{};
-	size_t rc = dt_parse_iso_date(civil_string,
-				      sizeof(civil_string) - 1,
-				      &dt_expected);
-	assert(rc > 1);
+	int64_t secs;
+	int64_t nanosecs;
+	int64_t ofs;
+	int rc = parse_datetime_extended(civil_string, sizeof(civil_string) - 1,
+					 &secs, &nanosecs, &ofs);
+	assert(rc == 0);
+	assert(nanosecs == 521345123);
 }
 
 void CDT_Parse1(benchmark::State& state) {
